b7e423aefbea363394f2285edd3ebebcae83a535
Ryan Porter <ryan.m.porter@gmail.com>
2021-09-19 09:00:04 -0700

    Update top level imports

--- src/maya_fn/__init__.py
+++ src/maya_fn/__init__.py
@@ -3,12 +3,20 @@
 A set of wrappers around a set of maya.cmds and maya.api behaviors.
 """
 
+import functools
+
+from maya import cmds
+
+import maya_fn.dg as dg  # noqa
+import maya_fn.dag as dag  # noqa
+import maya_fn.node as node  # noqa
+
+from maya_fn.plug import plug  # noqa
+
 __author__ = "Ryan Rorter"
 __version__ = "0.0.1"
 __license__ = "MIT"
 
-import maya_fn.node as node  # noqa
-import maya_fn.dag as dag  # noqa
 
 def undoable(name):
     def wrapper(func):
