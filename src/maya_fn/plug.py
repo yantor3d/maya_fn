314be87da7288a337f519162e24a69bf4b998804
Ryan Porter <ryan.m.porter@gmail.com>
2021-09-19 08:58:11 -0700

    Add upstream, downstream functions to plug.py

--- src/maya_fn/plug.py
+++ src/maya_fn/plug.py
@@ -33,29 +33,6 @@ def attr(plug):
     )
 
 
-def make(*args):
-    """Return the plug built up from the given arguments.
-
-    Args:
-        *args (str | int): Token(s) to build the plug name from.
-
-    Returns:
-        str
-    """
-
-    parts = []
-
-    for arg in args:
-        if isinstance(arg, int):
-            parts[-1] = "{}[{}]".format(parts[-1], arg)
-        elif isinstance(arg, six.string_types) and len(arg) == 1:
-            parts[-1] = "{}{}".format(parts[-1], arg)
-        else:
-            parts.append(arg)
-
-    return ".".join(parts)
-
-
 def destinations(plug):
     """Return the outputs of the given plug.
 
@@ -71,6 +48,19 @@ def destinations(plug):
     plugs = plug.connectedTo(False, True)
 
     return cmds.ls([p.name() for p in plugs], long=True)
+
+
+def downstream(plug):
+    """Return the nodes downstream the given plug.
+
+    Args:
+        plug (str): Path to an plug.
+
+    Returns:
+        list[str]
+    """
+
+    return [node(each) for each in destinations(plug)]
 
 
 def elements(plug):
@@ -114,6 +104,29 @@ def indices(plug):
         yield i
 
 
+def make(*args):
+    """Return the plug built up from the given arguments.
+
+    Args:
+        *args (str | int): Token(s) to build the plug name from.
+
+    Returns:
+        str
+    """
+
+    parts = []
+
+    for arg in args:
+        if isinstance(arg, int):
+            parts[-1] = "{}[{}]".format(parts[-1], arg)
+        elif isinstance(arg, six.string_types) and len(arg) == 1:
+            parts[-1] = "{}{}".format(parts[-1], arg)
+        else:
+            parts.append(arg)
+
+    return ".".join(parts)
+
+
 def node(plug):
     """Return the node of the given plug.
 
@@ -146,11 +159,33 @@ def source(plug):
     plug = maya_fn.api.get_plug(plug)
 
     plugs = plug.connectedTo(True, False)
+    plugs = [make(node(p), attr(p)) for p in plugs]
 
     if plugs:
-        return cmds.ls(plugs, long=True)[0]
+        return plugs[0]
     else:
         return None
+
+
+def split(plug):
+    """Return the node and attribute of the given plug."""
+
+    return node(plug), attr(plug)
+
+
+def upstream(plug):
+    """Return the node upstream the given plug.
+
+    Args:
+        plug (str): Path to an plug.
+
+    Returns:
+        list[str]
+    """
+
+    p = source(plug)
+
+    return p if p is None else source(p)
 
 
 def _get_array_plug(plug):
@@ -165,7 +200,7 @@ def _get_array_plug(plug):
 
 
 __functions__ = dict(
-    __call__=staticmethod(make),
+    __call__ = staticmethod(make),
     **{
         obj.__name__: staticmethod(obj)
         for obj in locals().values()
